6180eb81572dd80607b202fa78f51b8b59a18c15
Manender <Manendeer@gmail.com>
2021-05-26 18:23:18 +0500

    Перезалив
    Перезалил все файлы, так как некоторые отсутствовали

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<HealthController>() != null)
+        {
+            collision.gameObject.GetComponent<HealthController>().Health -= Mathf.Abs(GetComponent<Rigidbody2D>().mass + (GetComponent<Rigidbody2D>().velocity.x + GetComponent<Rigidbody2D>().velocity.y) + (collision.gameObject.GetComponent<Rigidbody2D>().velocity.x + collision.gameObject.GetComponent<Rigidbody2D>().velocity.y));
+            Destroy(this.gameObject);
+            print(collision.gameObject.GetComponent<HealthController>().Health);
+        }
+    }
+}
