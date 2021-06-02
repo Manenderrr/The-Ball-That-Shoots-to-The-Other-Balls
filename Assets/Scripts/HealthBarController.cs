308b791717a38e644c1c764dbdad50daa6632acd
Manender <Manendeer@gmail.com>
2021-06-02 21:47:25 +0500

    Добавлена полоска здоровья

---
+++ Assets/Scripts/HealthBarController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarController : MonoBehaviour
+{
+    public GameObject Placeholder;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<RectTransform>().localScale = new Vector3(Player.GetComponent<HealthController>().Health / Player.GetComponent<HealthController>().MaxHealth, 1, 0);
+
+        //GetComponent<RectTransform>().position = new Vector2(Placeholder.GetComponent<RectTransform>().position.x - Placeholder.GetComponent<RectTransform>().localScale.x + GetComponent<RectTransform>().localScale.x, Placeholder.GetComponent<RectTransform>().position.y);
+    }
+}
